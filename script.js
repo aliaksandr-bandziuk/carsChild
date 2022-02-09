43195dc85da51891db1c448cc96feb23ccccc062
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2022-02-09 20:22:24 +0300

    its work

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const cars = document.querySelectorAll('.block__item-text');
+const addHtml = () => {
+  const cars = document.querySelectorAll('.block__item-text');
 
-for (let i = 0; i < cars.length; i++) {
-  let carComplect = cars[i].querySelector('.complect');
-  let value = 'Комплектация: City Life';
-  console.log(carComplect);
-  if (carComplect.includes(value)) {
-    console.log(carComplect.includes(value));
+  for (let i = 0; i < cars.length; i++) {
+    let carComplect = cars[i].querySelector('.complect');
+    let complect = carComplect.innerHTML;
+    let value = 'Комплектация: City Life';
+    console.log(complect);
+    if (complect.includes(value)) {
+      console.log('привет');
+      carComplect.innerHTML += '<div><p><b>Скидки! Акции! Покупайте!</b></p></div>'
+    }
   }
 }
+
+setTimeout(addHtml, 3000);
